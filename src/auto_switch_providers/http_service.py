6d780fe88256eb24874e47cbee4a3822fca9a628
fuongz <hi@phuongphung.com>
2024-07-05 10:07:22 +0700

    feat(cache): add expires_in config

--- src/auto_switch_providers/http_service.py
+++ src/auto_switch_providers/http_service.py
@@ -71,6 +71,9 @@ class HttpService(object):
             endpoint = endpoint.format(**url_params_format)
 
         params_str = " - " + dumps(params) if params else ""
+        cache_url_query = urlencode(params or {})
+        if cache_url_query != "":
+            cache_url_query = f"?{cache_url_query}"
 
         for _ in range(total):
             try:
@@ -92,23 +95,23 @@ class HttpService(object):
                     if "auth_key" in cache_params:
                         cache_params.pop("auth_key")
 
-                    cache_url_query = urlencode(params or {})
                     cache_path = base64encode(cache_params)
-                    found = self.cache_service.client.exists(
-                        f"auto_switch_module:http_cached:{log_endpoint}:{cache_path}"
-                    )
+                    found = self.cache_service.exists([log_endpoint, cache_path])
                     if found:
-                        cached_response_text = self.cache_service.client.hget(
-                            f"auto_switch_module:http_cached:{log_endpoint}:{cache_path}",
+                        cached_response_text = self.cache_service.hget(
+                            [log_endpoint, cache_path],
                             "text",
                         )
-                        cached_response_status_code = self.cache_service.client.hget(
-                            f"auto_switch_module:http_cached:{log_endpoint}:{cache_path}",
+                        cached_response_status_code = self.cache_service.hget(
+                            [log_endpoint, cache_path],
                             "status_code",
                         )
-                        if int(cached_response_status_code) == 200:
+                        if (
+                            cached_response_status_code
+                            and int(cached_response_status_code) == 200
+                        ):
                             print(
-                                f"✅ [m] {log_endpoint}?{cache_url_query} (memory cache)"
+                                f"✅ [memory] {method}: {log_endpoint}{cache_url_query} (memory cache)"
                             )
                             return {
                                 "text": loads(cached_response_text),
@@ -126,42 +129,42 @@ class HttpService(object):
 
                 if response.status_code in status_force_list:
                     print(
-                        f"✅ [{response.elapsed.total_seconds() if response else None}s] {log_endpoint}?{cache_url_query} - retrying... ({_+1}) (code: wrong status_code)"
+                        f"✅ [{response.elapsed.total_seconds() if response else None}s] {method}: {log_endpoint}{cache_url_query} - retrying... ({_+1}) (code: wrong status_code)"
                     )
                     continue
 
                 if response.status_code == 200:
                     if self.cache_service is not None:
-                        self.cache_service.client.hset(
-                            f"auto_switch_module:http_cached:{log_endpoint}:{cache_path}",
+                        self.cache_service.hset(
+                            [log_endpoint, cache_path],
                             "text",
-                            dumps(response.text),
+                            response.text,
                         )
 
-                        self.cache_service.client.hset(
-                            f"auto_switch_module:http_cached:{log_endpoint}:{cache_path}",
+                        self.cache_service.hset(
+                            [log_endpoint, cache_path],
                             "status_code",
-                            dumps(response.status_code),
+                            response.status_code,
                         )
 
                 if response.status_code == 400:
                     print(
-                        f"🚸 [{timeout if timeout else None}s] {method}: {log_endpoint}?{cache_url_query} - stopped ({_+1}) (code: HTTPError) {params_str}"
+                        f"🚸 [{timeout if timeout else None}s] {method}: {log_endpoint}{cache_url_query} - stopped ({_+1}) (code: HTTPError) {params_str}"
                     )
                 else:
                     print(
-                        f"✅ [{response.elapsed.total_seconds() if response else None}s] GET: {log_endpoint}?{cache_url_query}"
+                        f"✅ [{response.elapsed.total_seconds() if response else None}s] {method}: {log_endpoint}{cache_url_query}"
                     )
 
                 return response
             except requests.exceptions.Timeout:
                 print(
-                    f"⛔️ [{timeout if timeout else None}] {method}: {log_endpoint}?{cache_url_query} - retrying... ({_+1}) (code: Timeout){params_str}"
+                    f"⛔️ [{timeout if timeout else None}] {method}: {log_endpoint}{cache_url_query} - retrying... ({_+1}) (code: Timeout){params_str}"
                 )
                 continue
             except requests.exceptions.ConnectionError:
                 print(
-                    f"⛔️ [{timeout if timeout else None}] {method}: {log_endpoint}?{cache_url_query} - stopped ({_+1}) (code: ConnectionError) {params_str}"
+                    f"⛔️ [{timeout if timeout else None}] {method}: {log_endpoint}{cache_url_query} - stopped ({_+1}) (code: ConnectionError) {params_str}"
                 )
                 pass
 
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -7,11 +7,22 @@ sys.path.append("./")
 from src.auto_switch_providers.auto_switch_providers import AutoSwitchProviders
 
 TEMPLATE_CONFIG = {
-    "googleapi": {"http_service": {"params": {"key": ""}}},
-    "proxiesapi": {"http_service": {"params": {"auth_key": ""}}},
+    # "googleapi": {"http_service": {"params": {"key": ""}}},
+    # "proxiesapi": {"http_service": {"params": {"auth_key": ""}}},
+    "serper": {
+        "http_service": {
+            "headers": {"X-API-KEY": "c50ed9f437790d262dfbe4e2bff5526ce1e2dcc5"}
+        }
+    },
 }
 
-CACHE_CONFIG = {"host": "127.0.0.1", "password": "", "port": 6379}
+CACHE_CONFIG = {
+    "host": "127.0.0.1",
+    "password": "r3d1s",
+    "port": 6379,
+    "database": "auto_switch_module:http_cached",
+    "expires_in": 2592000,
+}
 
 
 class TestSample(unittest.TestCase):
@@ -19,7 +30,15 @@ class TestSample(unittest.TestCase):
         response = AutoSwitchProviders(
             template_dir=f"{path.dirname(__file__)}/templates",
             config=TEMPLATE_CONFIG,
-        ).process({})
+            cache_config=CACHE_CONFIG,
+        ).process(
+            {
+                "query": "booking.com",
+                "limit": 100,
+                "page": 1,
+                "country": "VN",
+            }
+        )
         self.assertEqual(response, {})
 
 
